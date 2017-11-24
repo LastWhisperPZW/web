acc82dfe94bc29ec6bff53dde5fecc84f6919f77
panzhiwen <695802398@qq.com>
2017-11-24 12:26:38 +0800

    web sourcecode runnable

---
+++ fabfile.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Liao'
+
+'''
+Deployment toolkit.
+'''
+
+import os, re
+
+from datetime import datetime
+from fabric.api import *
+
+env.user = 'root'
+env.sudo_user = 'root'
+env.hosts = ['39.108.217.35']
+
+db_user = 'root'
+db_password = '950209'
+
+_TAR_FILE = 'dist-awesome.tar.gz'
+
+_REMOTE_TMP_TAR = '/tmp/%s' % _TAR_FILE
+
+_REMOTE_BASE_DIR = '/srv/awesome'
+
+def _current_path():
+    return os.path.abspath('.')
+
+def _now():
+    return datetime.now().strftime('%y-%m-%d_%H.%M.%S')
+
+def backup():
+    '''
+    Dump entire database on server and backup to local.
+    '''
+    dt = _now()
+    f = 'backup-awesome-%s.sql' % dt
+    with cd('/tmp'):
+        run('mysqldump --user=%s --password=%s --skip-opt --add-drop-table --default-character-set=utf8 --quick awesome > %s' % (db_user, db_password, f))
+        run('tar -czvf %s.tar.gz %s' % (f, f))
+        get('%s.tar.gz' % f, '%s/backup/' % _current_path())
+        run('rm -f %s' % f)
+        run('rm -f %s.tar.gz' % f)
+
+def build():
+    '''
+    Build dist package.
+    '''
+    includes = ['static', 'templates',  'favicon.ico', '*.py']
+    excludes = ['test', '.*', '*.pyc', '*.pyo']
+    local('rm -f dist/%s' % _TAR_FILE)
+    with lcd(os.path.join(_current_path(), 'www')):
+        cmd = ['tar', '--dereference', '-czvf', '../dist/%s' % _TAR_FILE]
+        cmd.extend(['--exclude=\'%s\'' % ex for ex in excludes])
+        cmd.extend(includes)
+        local(' '.join(cmd))
+
+def deploy():
+    newdir = 'www-%s' % _now()
+    run('rm -rf %s' % _REMOTE_TMP_TAR)
+    put('dist/%s' % _TAR_FILE, _REMOTE_TMP_TAR)
+    with cd(_REMOTE_BASE_DIR):
+        sudo('mkdir %s' % newdir)
+    with cd('%s/%s' % (_REMOTE_BASE_DIR, newdir)):
+        sudo('tar -xzvf %s' % _REMOTE_TMP_TAR)
+    with cd(_REMOTE_BASE_DIR):
+        sudo('rm -rf www')
+        sudo('ln -s %s www' % newdir)
+        sudo('chown www-data:www-data www')
+        sudo('chown -R www-data:www-data %s' % newdir)
+    with settings(warn_only=True):
+        sudo('supervisorctl stop awesome')
+        sudo('supervisorctl start awesome')
+        sudo('/etc/init.d/nginx reload')
+
+
